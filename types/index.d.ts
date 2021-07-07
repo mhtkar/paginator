4ac0aed5dc45b966f1ccc11f022e9e932f301e70
mohit <karkarmohit@gmail.com>
2021-07-07 15:44:10 +0530

    declaration file fixed

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,9 @@
 //types
-export declare function paginator(
+declare function paginator(
     totalPosts : number,
     currentPage :number,
     postsPerPage :number,
     maximumPage : number
   ) :number[]
+
+export = paginator
